4ff06ec2c505c762d5c4f5295f2210a1981e5aea
DarkShark <darksharkbite21@gmail.com>
2026-09-23 22:26:20 +0530

    chore: bring back type definitions

--- include/types.hpp
+++ include/types.hpp
@@ -42,57 +42,57 @@ ENMA_NS_START
 /* Boolean Vector Types */
 
 using  bvec1 = bln8;
-//struct bvec2;
-//struct bvec3;
-//struct bvec4;
+struct bvec2;
+struct bvec3;
+struct bvec4;
 
 /* Integer Vector Types */
 
 using sivec1 = sint32;
-//struct sivec2;
-//struct sivec3;
-//struct sivec4;
+struct sivec2;
+struct sivec3;
+struct sivec4;
 
 /* Unsigned Integer Vector Types */
 
 using uvec1 = uint32;
-//struct uvec2;
-//struct uvec3;
-//struct uvec4;
+struct uvec2;
+struct uvec3;
+struct uvec4;
 
 /* Single-Precision Floating-Point Vector Types */
 
 using sfvec1 = flt32;
-//struct sfvec2;
-//struct sfvec3;
-//struct sfvec4;
+struct sfvec2;
+struct sfvec3;
+struct sfvec4;
 
 /* Double-Precision Floating-Point Vector Types */
 
 using dfvec1 = flt64;
-//struct dfvec2;
-//struct dfvec3;
-//struct dfvec4;
+struct dfvec2;
+struct dfvec3;
+struct dfvec4;
 
 /* Single-Precision Floating-Point Matrix Types */
 
-//struct sfmat2x2;
-//struct sfmat2x3;
-//struct sfmat2x4;
+struct sfmat2x2;
+struct sfmat2x3;
+struct sfmat2x4;
 
-//using sfmat2 = sfmat2x2;
+using sfmat2 = sfmat2x2;
 
-//struct sfmat3x2;
-//struct sfmat3x3;
-//struct sfmat3x4;
+struct sfmat3x2;
+struct sfmat3x3;
+struct sfmat3x4;
 
-//using sfmat3   = sfmat3x3;
+using sfmat3 = sfmat3x3;
 
-//struct sfmat4x2;
-//struct sfmat4x3;
-//struct sfmat4x4;
+struct sfmat4x2;
+struct sfmat4x3;
+struct sfmat4x4;
 
-//using sfmat4   = sfmat4x4;
+using sfmat4 = sfmat4x4;
 
 /* Floating-Point Quaternions */
 
